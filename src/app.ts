c0a7ee20327b16c2c13d8b06e518eb40e5fe5946
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2022-11-21 11:25:56 +0900

    set 2

--- src/app.ts
+++ src/app.ts
@@ -16,9 +16,9 @@ viewpt.onloadeddata = () => {
 
 video.onloadeddata = () => {
   const black = [122, 122, 122, 255];
-  const ratio = 10;
-  const width = 1200;
-  const height = 600;
+  const ratio = 100;
+  const width = 1024;
+  const height = 500;
   render(video, width, height, ratio, black);
   canvas.width = width + ratio * 4;
   canvas.height = height + ratio * 4;
@@ -28,7 +28,7 @@ video.onloadeddata = () => {
     "video"
   ) as HTMLElement as HTMLVideoElement;
 
-  // makeVideo.srcObject = stream;
+  makeVideo.srcObject = stream;
   makeVideo.autoplay = true;
   makeVideo.setAttribute("id", "maked-video");
 
@@ -46,7 +46,7 @@ function render(
   ratio: number,
   coveredColor: number[]
 ) {
-  // console.time();
+  console.time();
   ctx.drawImage(video, 0, 0, width, height);
   const [red, green, blue, alpha] = coveredColor;
   const widthNumbers = (width * height * 4) / height;
@@ -54,9 +54,11 @@ function render(
   const rowRatio = widthNumbers + sideRatio;
 
   const array = new Array(rowRatio * ratio).fill(0);
-  console.time("좌우 영상");
-  const data: any[] = [...ctx.getImageData(0, 0, width, height).data];
-  console.timeEnd("좌우 영상");
+  const totalData = ctx.getImageData(0, 0, width, height).data;
+  const data: any[] = [];
+  for (let i = 0; i < totalData.length; i++) {
+    data[i] = totalData[i];
+  }
   const fixedData = [];
   for (let i = 0; i < ratio; i += 1) {
     fixedData.push(red);
@@ -78,6 +80,7 @@ function render(
       data[i + widthNumbers / 2],
     ]);
   }
+
   // console.timeEnd("좌우 영상");
   const createdUint8Array = [];
 
@@ -85,31 +88,44 @@ function render(
 
   for (let i = 0; i < data.length; i++) {
     if (typeof data[i] === "object") {
-      createdUint8Array.push(...data[i]);
+      for (let j = 0; j < data[i].length; j++) {
+        createdUint8Array.push(data[i][j]);
+      }
       continue;
     }
     createdUint8Array.push(data[i]);
   }
 
   // console.timeEnd("이중 배열");
-  // console.time("위아래 여백");
-
   for (let i = 0; i < array.length - 3; i += 4) {
     array[i] = red;
     array[i + 1] = green;
     array[i + 2] = blue;
     array[i + 3] = alpha;
   }
-  // console.timeEnd("위아래 여백");
   // console.time("최종 병합");
-  const testing = [...array, ...createdUint8Array, ...array];
+  // const testing = [...array, ...createdUint8Array, ...array];
+
+  const testing = [];
+  for (let i = 0; i < array.length; i++) {
+    testing[i] = array[i];
+  }
+
+  for (let i = 0; i < createdUint8Array.length; i++) {
+    testing[testing.length] = createdUint8Array[i];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    testing[testing.length] = array[i];
+  }
+
   // console.timeEnd("최종 병합");
   const arr = new Uint8ClampedArray(testing);
   const imageData = new ImageData(arr, width + ratio * 4, height + ratio * 2);
   ctx.putImageData(imageData, 0, 0);
 
-  // console.timeEnd();
+  console.timeEnd();
   setTimeout(() => {
     render(video, width, height, ratio, coveredColor);
-  }, 1000 / 60);
+  }, 1000 / 30);
 }
